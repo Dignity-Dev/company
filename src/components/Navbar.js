654b0c7526a91618497939946ba1e7bf30d1ae82
Dignity Dev <bashirhaadiy@gmail.com>
2022-02-09 22:54:05 +0100

    new change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return (
-    <>
-      <nav className="navbar">
-              <div className="navbar-container">
-                  
-        </div>
-      </nav>
-    </>
-  );
+    const [click, setClick] = useState(false);
+
+    const handClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    return (
+        <>
+            <nav className="navbar" >
+                <div className="navbar-container" >
+                    <Link to="/" className="navbar-logo" >
+                        Dignity
+                        <i classname="fa fa-envelope" />
+                    </Link>
+                    <div classname="menu-icon" onClick={handClick}>
+                        <i classname={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul classname={click ? 'nav-menu active' : 'nav:menu'}>
+                        <li classname='nav-item'>
+                            <Link to='/' classname='nav-links' onClick={closeMobileMenu}> Home </Link>
+                        </li>
+                        <li classname='nav-item'>
+                            <Link to='/products' classname='nav-links' onClick={closeMobileMenu}> Products </Link>
+                        </li>
+                        <li classname='nav-item'>
+                            <Link to='/services' classname='nav-links' onClick={closeMobileMenu}> Services </Link>
+                        </li>
+                        <li classname='nav-item'>
+                            <Link to='/sign-up' classname='nav-links' onClick={closeMobileMenu}> Sign Up </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
